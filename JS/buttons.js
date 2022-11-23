cf0458022e956c94b18d83ee19babe397b7d76e2
Ro005 <84298912+Roberto-Dantas@users.noreply.github.com>
2022-11-23 06:34:24 -0300

    Update buttons.js

--- JS/buttons.js
+++ JS/buttons.js
@@ -31,18 +31,18 @@ function clicked() {
                 actived[i] = false    
             }
         }
-        month1.style.height= '60px'
-        month2.style.height= '60px'
-        month3.style.height= '60px'
-        month4.style.height= '60px'
-        month5.style.height= '60px'
-        month6.style.height= '60px'
-        month7.style.height= '60px'
-        month8.style.height= '60px'
-        month9.style.height= '60px'
-        month10.style.height= '60px'
-        month11.style.height= '60px'
-        month12.style.height= '60px'
+        month1.style.height= '90px'
+        month2.style.height= '90px'
+        month3.style.height= '90px'
+        month4.style.height= '90px'
+        month5.style.height= '90px'
+        month6.style.height= '90px'
+        month7.style.height= '90px'
+        month8.style.height= '90px'
+        month9.style.height= '90px'
+        month10.style.height= '90px'
+        month11.style.height= '90px'
+        month12.style.height= '90px'
     }
     
     month1.onclick = function() {
@@ -50,208 +50,223 @@ function clicked() {
          oneAtTime()         
          if(actived[number]) {
              this.style.transition="0.75s"
-             this.style.height= '60px'
+             this.style.height= '90px'
              actived[number] = false
              writingInserting()
          }else {
              this.style.transition="0.75s"
-             this.style.height= '80px'
+             this.style.height= '70px'
              actived[number] = true
              variable = true
              writingInserting(number+1)
          }
+         window.scrollTo(0, 0);
      };
     month2.onclick = function() {
          number = 1
          oneAtTime()
          if(actived[number]) {
              this.style.transition="0.75s"
-             this.style.height= '60px'
+             this.style.height= '90px'
              actived[number] = false
              writingInserting()
          }else {
              this.style.transition="0.75s"
-             this.style.height= '80px'
+             this.style.height= '70px'
              actived[number] = true
              variable = true
              writingInserting(number+1)
          }
+         window.scrollTo(0, 0);
     };
     month3.onclick = function() {
          number = 2
          oneAtTime()
          if(actived[number]) {
              this.style.transition="0.75s"
-             this.style.height= '60px'
+             this.style.height= '90px'
              actived[number] = false
              writingInserting()
          }else {
              this.style.transition="0.75s"
-             this.style.height= '80px'
+             this.style.height= '70px'
              actived[number] = true
              variable = true
              writingInserting(number+1)
          }
+         window.scrollTo(0, 0);
     };
     month4.onclick = function() {
          number = 3
          oneAtTime()
          if(actived[number]) {
              this.style.transition="0.75s"
-             this.style.height= '60px'
+             this.style.height= '90px'
              actived[number] = false
              writingInserting()
          }else {
              this.style.transition="0.75s"
-             this.style.height= '80px'
+             this.style.height= '70px'
              actived[number] = true
              variable = true
              writingInserting(number+1)
          }
+         window.scrollTo(0, 0);
     };
     month5.onclick = function() {
          number = 4
          oneAtTime()
          if(actived[number]) {
              this.style.transition="0.75s"
-             this.style.height= '60px'
+             this.style.height= '90px'
              actived[number] = false
              writingInserting()
          }else {
              this.style.transition="0.75s"
-             this.style.height= '80px'
+             this.style.height= '70px'
              actived[number] = true
              variable = true
              writingInserting(number+1)
          }
+         window.scrollTo(0, 0);
     };
     month6.onclick = function() {
          number = 5
          oneAtTime()
          if(actived[number]) {
              this.style.transition="0.75s"
-             this.style.height= '60px'
+             this.style.height= '90px'
              actived[number] = false
              writingInserting()
          }else {
              this.style.transition="0.75s"
-             this.style.height= '80px'
+             this.style.height= '70px'
              actived[number] = true
              variable = true
              writingInserting(number+1)
          }
+         window.scrollTo(0, 0);
     };
     month7.onclick = function() {
          number = 6
          oneAtTime()
          if(actived[number]) {
              this.style.transition="0.75s"
-             this.style.height= '60px'
+             this.style.height= '90px'
              actived[number] = false
              writingInserting()
          }else {
              this.style.transition="0.75s"
-             this.style.height= '80px'
+             this.style.height= '70px'
              actived[number] = true
              variable = true
              writingInserting(number+1)
          }
+         window.scrollTo(0, 0);
     };
     month8.onclick = function() {
          number = 7
          oneAtTime()
          if(actived[number]) {
              this.style.transition="0.75s"
-             this.style.height= '60px'
+             this.style.height= '90px'
              actived[number] = false
              writingInserting()
          }else {
              this.style.transition="0.75s"
-             this.style.height= '80px'
+             this.style.height= '70px'
              actived[number] = true
              variable = true
              writingInserting(number+1)
          }
+         window.scrollTo(0, 0);
     };
     month9.onclick = function() {
          number = 8
          oneAtTime()
          if(actived[number]) {
              this.style.transition="0.75s"
-             this.style.height= '60px'
+             this.style.height= '90px'
              actived[number] = false
              writingInserting()
          }else {
              this.style.transition="0.75s"
-             this.style.height= '80px'
+             this.style.height= '70px'
              actived[number] = true
              variable = true
              writingInserting(number+1)
          }
+         window.scrollTo(0, 0);
     };
     month10.onclick = function() {
          number = 9
          oneAtTime()
          if(actived[number]) {
              this.style.transition="0.75s"
-             this.style.height= '60px'
+             this.style.height= '90px'
              actived[number] = false
              writingInserting()
          }else {
              this.style.transition="0.75s"
-             this.style.height= '80px'
+             this.style.height= '70px'
              actived[number] = true
              variable = true
              writingInserting(number+1)
          }
+         window.scrollTo(0, 0);
     };
     month11.onclick = function() {
          number = 10
          oneAtTime()
          if(actived[number]) {
              this.style.transition="0.75s"
-             this.style.height= '60px'
+             this.style.height= '90px'
              actived[number] = false
              writingInserting()
          }else {
              this.style.transition="0.75s"
-             this.style.height= '80px'
+             this.style.height= '70px'
              actived[number] = true
              variable = true
              writingInserting(number+1)
          }
+         window.scrollTo(0, 0);
     };
     month12.onclick = function() {
          number = 11
          oneAtTime()
          if(actived[number]) {
              this.style.transition="0.75s"
-             this.style.height= '60px'
+             this.style.height= '90px'
              actived[number] = false
              writingInserting()
          }else {
              this.style.transition="0.75s"
-             this.style.height= '80px'
+             this.style.height= '70px'
              actived[number] = true
              variable = true
              writingInserting(number+1)
          }
+         window.scrollTo(0, 0);
     };
 }
 
 let variable = false
 
 function dataEntry(titleMonth, textMonth) {
-    const theTitle = document.querySelector('.chosen-month h2')
+    const theTitle = document.querySelector('.chosen-month h2');
+    let textContent = document.querySelector('.text');
+    
     if(variable){
         if(actived[number]) {
             theTitle.innerText = titleMonth
+            textContent.style.border= "3px solid #000"
             variable = false
         }
     }else{
-        let textContent = document.querySelector('.text') ;
         theTitle.innerText = "Escolha um dos meses acima";
         textContent.style.backgroundColor= "";
+        textContent.style.border=""
     }
     
 }
